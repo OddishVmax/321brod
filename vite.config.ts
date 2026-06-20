6594a8be589433ed69e657079027efda6786361e
OddishVmax <sigmund.m.berntsen@gmail.com>
2026-06-20 10:57:52 +0200

    fix: pappa hjelper med github

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: './',
+  base: '/321brod/',
   publicDir: 'public',
   build: {
     outDir: 'docs',
